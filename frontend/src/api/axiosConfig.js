2820a9ddc403b4261eb364e06fcaac9a450ea577
vasu580 <natinavasu9@gmal.com.com>
2026-09-01 21:41:40 +0530

    Use Vercel API proxy

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://vnexatrendz-fullstack-ecommerce.onrender.com/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
